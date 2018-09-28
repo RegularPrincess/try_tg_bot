c625da8de9ce4462cbe2dd5198544bcadd31fc44
JMFedorov <jmfedorov@gmail.com>
2018-09-28 12:06:27 +0300

    add first msg

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import telebot.types as types
 bot = telebot.TeleBot("645100799:AAHr08yGqhY8PxAjeSJSdPiUZ-D2MgcB3i8")
 
 
+
+# Обработчик команд '/start' и '/help'.
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+    bot.send_message(message.from_user.id, "Здравствуйте, вас приветствует тестовый бот, "
+                                           "который поможет вам опросить клиентов. ")
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    itembtn1 = types.KeyboardButton('Да')
+    itembtn2 = types.KeyboardButton('Нет')
+    markup.add(itembtn1, itembtn2)
+    bot.send_message(message.from_user.id, "Вам будет задан ряд вопросов, вы готовы?", reply_markup=markup)
+
+
+
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     if message.text == "Да" or message.text == "да":
@@ -18,19 +32,6 @@ def handle_text(message):
 
 
 bot.polling(none_stop=True, interval=0)
-
-
-# Обработчик команд '/start' и '/help'.
-@bot.message_handler(commands=['start', 'help'])
-def handle_start_help(message):
-    bot.send_message(message.from_user.id, "Здравствуйте, вас приветствует тестовый бот, "
-                                           "который поможет вам опросить клиентов. ")
-    markup = types.ReplyKeyboardMarkup(row_width=1)
-    itembtn1 = types.KeyboardButton('Да')
-    itembtn2 = types.KeyboardButton('Нет')
-    markup.add(itembtn1, itembtn2)
-    bot.send_message(message.from_user.id, "Вам будет задан ряд вопросов, вы готовы?", reply_markup=markup)
-
 
 # Обработчик для документов и аудиофайлов
 @bot.message_handler(content_types=['document', 'audio'])
